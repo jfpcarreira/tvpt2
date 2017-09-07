8c6ba28e73f2e3b6b63d6f37343345126b4f128d
Joao Carreira <joao.carreira@videotron.com>
2017-09-07 11:06:53 -0400

    Adicionado novo export nos ficheiros environment

--- client/src/environments/environment.prod.ts
+++ client/src/environments/environment.prod.ts
@@ -1,3 +1,15 @@
 export const environment = {
-  production: true
+    production: true
+  , API_DOMAIN: 'http://localhost:3000'
+  , CLIENT_API_URL: '/api/client/'
+  , SERVICE_API_URL: '/api/service/'
+  , CURRENCY_API_URL: '/api/currency/'
+  , TRANSLATE_API_URL: '/api/translate'
+};
+
+export const API_URLS = {
+    CLIENT: environment.API_DOMAIN + environment.CLIENT_API_URL
+  , SERVICE: environment.API_DOMAIN + environment.SERVICE_API_URL
+  , CURRENCY: environment.API_DOMAIN + environment.CURRENCY_API_URL
+  , TRANSLATE: environment.API_DOMAIN + environment.TRANSLATE_API_URL
 };
